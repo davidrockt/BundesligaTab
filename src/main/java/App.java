cba096f83d5b2e99695c3eae8682c5cb4299c9a1
David <hi-davidw@web.de>
2019-06-29 12:52:09 +0200

    Websocket Steps

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,20 @@
 import io.javalin.Javalin;
+import io.javalin.websocket.WsSession;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class App {
     public static void main(String[] args) {
-        Javalin app = Javalin.create().enableStaticFiles("/public").start(7000);
+        Javalin app = Javalin.create()
+                .enableStaticFiles("/public")
+                .start(7000);
+
+        Map<WsSession, String> sessions = new ConcurrentHashMap<>();
+
         Country de = new Country("Deutschland"),gb = new Country("England"),
                 es = new Country("Spanien"), br = new Country("Brasilien");
         Map<String, Country> countries = new HashMap<String, Country>(){{
@@ -20,8 +27,27 @@ public class App {
         Match match = new Match(de, br, 7, 1);
         Match match2 = new Match(gb, es, 3, 2);
 
-        app.ws("/websocket/:path", ws -> {
-            ws.onConnect(ctx -> System.out.println("Connected"));
+        app.ws("/livematch", ws -> {
+            ws.onConnect(session -> {
+                System.out.println("Connected");
+                
+                // Kopie vom Projekt "connect4" (Nur 2 verschiedene Sessions möglich)
+                if(sessions.size() == 1) {
+                    if(sessions.containsValue("2")){
+                        sessions.put(session, "1");
+                    }
+                    else {
+                        sessions.put(session, "2");
+                    }
+                }
+                if(sessions.size() == 0) {
+                    sessions.put(session, "1");
+                }
+                // Ende Kopie
+            });
+            ws.onClose((session, status, message) -> {
+                sessions.remove(session);
+            });
         });
 
         app.get("/start", ctx -> ctx.result(table.toString()));
